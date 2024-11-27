cc9a5db7ae200153677bf82c2beca69a78f470af
Oshith Roshantha <oedirisuriya@gmail.com>
2024-11-27 22:31:34 +0530

    enhance login page layout and styles, add swipe functionality

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,22 @@
 import './Styles/Login.css'
+import mainLogo from '../assets/Icons/main-logo.png'
+import { useState } from 'react';
 
 export default function Login() {
+  const [swiped, setSwiped] = useState(false);
+  const handleSwipe = () => {
+    setSwiped(!swiped);
+  };
   return (
-    <div>
-        <div className='main-container'>Welcome</div>
+    <div className={`main-container ${swiped ? 'swiped' : ''}`}>
+        <div className='left-side'>
+            <img src={mainLogo} className='main-logo' alt='Main Logo'/>
+            <h1 className='main-hero'>A place for meaningful conversations</h1>
+            <h1 className="display-4 main-sub">Sign in to stay connected with your friends and team.</h1>
+        </div>
+        <div className='right-side'>
+
+        </div>
     </div>
   )
 }
