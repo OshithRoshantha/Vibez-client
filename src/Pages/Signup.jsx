1a5e9de45d89dc29da01d2b2a8089ae276365688
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-07 11:50:10 +0530

    replace StepperElement with SignupElement component; update Signup page layout and styles

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './Styles/Signup.css'
-import Stepper from '@/Components/StepperElement'
+import SignupElement from '@/Components/SignupElement'
 
 export default function Signup() {
   return (
     <div className='second-container'>
+      <div className='header'>
+        <p class="h1 signup-main">Sign Up</p>
+        <p class="display-6 signup-sub">Let's get started. Are you ready to part of something great?</p>
+      </div>
       <div className='signup-container'>
-        <Stepper/>
+        <SignupElement/>
       </div>
     </div>
   )
