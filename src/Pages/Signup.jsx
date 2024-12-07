75ff2d69a0d161aa2f7d6c5d55432f09c381768e
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-07 10:31:30 +0530

    add Stepper component and styles; update Signup page layout with new container

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './Styles/Signup.css'
+import Stepper from '@/Components/StepperElement'
 
 export default function Signup() {
   return (
-    <div className='second-container'></div>
+    <div className='second-container'>
+      <div className='signup-container'>
+        <Stepper/>
+      </div>
+    </div>
   )
 }
