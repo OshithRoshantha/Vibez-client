ac428935f9258b85b552584c7aa5f433bab3a4d4
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-07 12:07:48 +0530

    enhance Signup page layout; add logo and adjust header styling for better alignment

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './Styles/Signup.css'
 import SignupElement from '@/Components/SignupElement'
+import mainLogo from '../assets/Icons/main-logo.png'
 
 export default function Signup() {
   return (
     <div className='second-container'>
       <div className='header'>
-        <p class="h1 signup-main">Sign Up</p>
-        <p class="display-6 signup-sub">Let's get started, Sign up and keep the conversation flowing.</p>
+        <div className='header-right'>
+          <p class="h1 signup-main">Sign Up</p>
+          <p class="display-6 signup-sub">Let's get started, Sign up and keep the conversation flowing.</p>
+        </div>
+        <div className='logo'>
+          <img src={mainLogo} className='main-logo-signup' alt='Main Logo'/>
+        </div>
       </div>
       <div className='signup-container'>
         <SignupElement/>
