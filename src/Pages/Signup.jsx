80a918de42bbdf12be85512ae9489c19348c676e
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-07 11:52:05 +0530

    update Signup page subtitle for improved clarity and engagement

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -7,7 +7,7 @@ export default function Signup() {
     <div className='second-container'>
       <div className='header'>
         <p class="h1 signup-main">Sign Up</p>
-        <p class="display-6 signup-sub">Let's get started. Are you ready to part of something great?</p>
+        <p class="display-6 signup-sub">Let's get started, Sign up and keep the conversation flowing.</p>
       </div>
       <div className='signup-container'>
         <SignupElement/>
