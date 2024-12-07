121f2a96e6f7dc3e977ae79b5d8185aa4633a696
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-07 12:11:55 +0530

    add navigation to Sign In on Signup page; adjust row gap in styles for better layout

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import './Styles/Signup.css'
 import SignupElement from '@/Components/SignupElement'
 import mainLogo from '../assets/Icons/main-logo.png'
+import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
+  const navigate = useNavigate();
+  function navSignin() {
+      navigate('/');
+  }
+
   return (
     <div className='second-container'>
       <div className='header'>
         <div className='header-right'>
           <p class="h1 signup-main">Sign Up</p>
-          <p class="display-6 signup-sub">Let's get started, Sign up and keep the conversation flowing.</p>
+          <p class="display-6 signup-sub">Let's get started, Sign up and keep the conversation flowing.</p><br></br>
+          <p>Already have an account? <a className='signup-link' onClick={() => {navSignin();}}>Sign In</a></p>
         </div>
         <div className='logo'>
           <img src={mainLogo} className='main-logo-signup' alt='Main Logo'/>
