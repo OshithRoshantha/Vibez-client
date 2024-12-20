e65131dba9494e8d27945f758d71cb8bd4f741b1
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-20 21:15:47 +0530

    feat: add delete confirmation menu to YourListings component

--- src/Components/YourListings.jsx
+++ src/Components/YourListings.jsx
@@ -1,7 +1,37 @@
 import './Styles/Column2.css'
+import { useState} from 'react';
 
 export default function YourListings({showEditListingMenu}) {
+  const [deleteMenu, setDeleteMenu] = useState(true);
+
+  function showDeleteMenu() {
+      setDeleteMenu(true);
+  }
+
+  function hideDeleteMenu() {
+      setDeleteMenu(false);
+  }
+
   return (  
+    <div>
+        {deleteMenu && <div>
+            <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+              <div className="bg-card rounded-lg p-6 w-80">
+                <div className='text-center'>
+                    <h2 className="text-lg font-semibold text-foreground">Delete this listing?</h2>
+                    <p className="text-muted-foreground mt-2">Are you sure that you want to delete this listing?</p>
+                </div>
+                <div style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
+                  <p className="text-muted-foreground mt-2">LKR57,500</p>
+                  <p className="text-muted-foreground">🛒 Phones</p>
+                </div>
+                <div>
+                  <button onClick={hideDeleteMenu} className="bg-primary text-primary-foreground px-4 py-1 rounded-lg w-full mt-3 mb-2">Delete</button>
+                  <button onClick={hideDeleteMenu} className="bg-muted text-muted-foreground px-4 py-1 border-none hover:bg-gray-300 w-full">Cancel</button>
+                </div>
+              </div>
+            </div>
+          </div>}
         <div className="w-full bg-card rounded-lg py-2" style={{display:'flex',columnGap:'5%',justifyContent:'center',alignItems:'center'}}>
         <img src="https://placehold.co/300x200" alt="9-cube shelf" className="your-listning-img" />
         <div>
@@ -10,9 +40,10 @@ export default function YourListings({showEditListingMenu}) {
             <p className="text-muted-foreground text-sm">Listed on 6/19</p>
             <div className="flex space-x-2 mt-1">
                 <button onClick={showEditListingMenu} className="bg-primary text-white hover:bg-secondary/80 px-2 py-1 rounded-lg text-sm"><i className="bi bi-pencil-square"></i>&nbsp;Edit listing</button>
-                <button className="bg-muted text-muted-foreground px-2 py-1 border-none hover:bg-gray-300 text-sm"><i className="bi bi-trash"></i>&nbsp;Delete listing</button>
+                <button onClick={showDeleteMenu} className="bg-muted text-muted-foreground px-2 py-1 border-none hover:bg-gray-300 text-sm"><i className="bi bi-trash"></i>&nbsp;Delete listing</button>
             </div>
         </div>
         </div>
+    </div>
   )
 }
