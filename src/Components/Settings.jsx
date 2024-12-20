6bf957f5dc2554552146ddc652952ca3e33b0f69
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-20 22:28:44 +0530

    feat: enhance Settings component with Account Settings header for better user experience

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -3,6 +3,10 @@ import './Styles/Column2.css'
 
 export default function Settings() {
   return (
-    <div>Settings</div>
+    <div>
+        <div className="border-r border-border p-4 chats-column">
+            <h2 className="text-lg font-semibold column-header">Account Settings</h2>
+        </div>
+    </div>
   )
 }
