ebc892c47644aeeeec95e26d07ab3b533f72195b
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-20 14:50:27 +0530

    feat: add YourListings component and integrate it into Marketplace for user listings management

---
+++ src/Components/YourListings.jsx
@@ -0,0 +1,18 @@
+import './Styles/Column2.css'
+
+export default function YourListings() {
+  return (  
+        <div className="w-full bg-card rounded-lg py-2" style={{display:'flex',columnGap:'2%',justifyContent:'center',alignItems:'center'}}>
+        <img src="https://placehold.co/300x200" alt="9-cube shelf" className="your-listning-img" />
+        <div>
+            <h2 className="text-sm font-semibold mt-2">9-cube shelf</h2>
+            <p className="text-lg font-bold text-primary">$10</p>
+            <p className="text-muted-foreground text-sm">Listed on 6/19</p>
+            <div className="flex space-x-2 mt-1">
+                <button className="bg-primary text-white hover:bg-secondary/80 px-2 py-1 rounded-lg text-sm"><i className="bi bi-pencil-square"></i>&nbsp;Edit listning</button>
+                <button className="bg-muted text-muted-foreground px-2 py-1 border-none hover:bg-gray-300 text-sm"><i className="bi bi-trash"></i>&nbsp;Delete listning</button>
+            </div>
+        </div>
+        </div>
+  )
+}
