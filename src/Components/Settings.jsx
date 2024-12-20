1284c63cc99cb9520ef51a8651bc8487824e5ad3
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-20 22:37:16 +0530

    feat: add additional options to Settings component for enhanced user customization

--- src/Components/Settings.jsx
+++ src/Components/Settings.jsx
@@ -6,6 +6,24 @@ export default function Settings() {
     <div>
         <div className="border-r border-border p-4 chats-column">
             <h2 className="text-lg font-semibold column-header">Account Settings</h2>
+            <ul className="space-y-2 mt-5">
+                <li className="flex items-center p-2 rounded-lg hover:bg-muted transition" style={{cursor: 'pointer'}}>
+                    <i className="bi bi-gear-fill text-2xl"></i>&nbsp;&nbsp;
+                    <span>Settings</span>
+                </li>
+                <li className="flex items-center p-2 rounded-lg hover:bg-muted transition" style={{cursor: 'pointer'}}>
+                    <i className="bi bi-unlock-fill text-2xl"></i>&nbsp;&nbsp;
+                    <span>Privacy Centre</span>
+                </li>
+                <li className="flex items-center p-2 rounded-lg hover:bg-muted transition" style={{cursor: 'pointer'}}>
+                    <i className="bi bi-moon-fill text-2xl"></i>&nbsp;&nbsp;
+                    <span>Dark mode</span>
+                </li>
+                <li className="flex items-center p-2 rounded-lg hover:bg-muted transition" style={{cursor: 'pointer'}}>
+                    <i className="bi bi-globe2 text-2xl"></i>&nbsp;&nbsp;
+                    <span>App language</span>
+                </li>
+            </ul>
         </div>
     </div>
   )
