0cfb76f05a9c73e2f9c7c067354f516c8195392b
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-24 11:59:09 +0530

    feat: update GroupReceiveMessage and GroupSendMessage components to include sender name and adjust message styling

--- src/Components/GroupChat.jsx
+++ src/Components/GroupChat.jsx
@@ -16,11 +16,11 @@ export default function GroupChat() {
             </div>
         </div>
         <div className="p-4" style={{height:'78vh', overflowY:'auto', scrollbarWidth:'none'}}>
-            <GroupReceiveMessage senderName={'sendUser'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
-            <GroupReceiveMessage senderName={'sendUser'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAA'}/>
-            <GroupReceiveMessage senderName={'sendUser'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
+            <GroupReceiveMessage senderName={'sendUser 01'} time={'00:26'} message={'Simple AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
+            <GroupReceiveMessage senderName={'sendUser 02'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAA'}/>
+            <GroupReceiveMessage senderName={'sendUser 01'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
             <GroupSendMessage time={'00:26'} message={'send message'}/>
-            <GroupReceiveMessage senderName={'sendUser'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
+            <GroupReceiveMessage senderName={'sendUser 01'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
         </div>
         <div className=" px-4 py-3  border-t border-border bg-card" style={{display:'flex', alignItems:'center',columnGap:'1rem'}}>
             <input type="text" placeholder="Type a message" className="w-full p-2 rounded-lg bg-input text-black" />
