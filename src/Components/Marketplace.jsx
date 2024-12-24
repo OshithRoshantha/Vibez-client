90f40c82d0ffa2246177b1713984ca3cb2b356bf
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-24 22:30:29 +0530

    style: fix spacing in Marketplace component layout

--- src/Components/Marketplace.jsx
+++ src/Components/Marketplace.jsx
@@ -94,7 +94,7 @@ export default function Marketplace() {
 
   return (
     <div>
-        <div className="border-r border-border p-4 chats-column">
+        <div className="border-r border-border p-4 chats-column" >
             <h2 className="text-lg font-semibold column-header">Marketplace</h2>
             <input type="text" placeholder="What do you want to buy?" className="placeholder:text-gray-500 bg-gray-200 text-gray-500 w-full px-4 py-2 mb-4 focus:outline-none focus:border-none" style={{borderRadius:'20px'}} />
             <i className="bi text-gray-500 absolute text-2xl bi-search" style={{marginLeft:'-3%', marginTop:'0.2%'}}></i>
