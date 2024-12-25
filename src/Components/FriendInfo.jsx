f7933d6065b8891f3aef49212c01a68b23a01827
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-25 14:11:09 +0530

    style: implement favorite toggle functionality in FriendInfo component

--- src/Components/FriendInfo.jsx
+++ src/Components/FriendInfo.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import {useState} from 'react'
 
 export default function FriendInfo() {
+  const [isFavorite, setIsFavorite] = useState(true);
+
+  function setToFavorite() {
+    setIsFavorite(true);
+  }
+
+  function unsetFromFavorite() {
+    setIsFavorite(false);
+  }
+
+  function toggleFavorite(){
+    if(isFavorite){
+      unsetFromFavorite();
+    } else {
+      setToFavorite();
+    }
+  }
+
+  var user = "testUser"
   return (
     <div>
       <div className="border-r border-border p-4 info-column">
@@ -9,20 +28,24 @@ export default function FriendInfo() {
             <div className="bg-zinc-300 rounded-full flex items-center justify-center mb-4" style={{backgroundColor:'red', width:'150px', height:'150px'}}>
               
             </div>
-            <h2 className="text-xl font-semibold text-foreground">My Airtel</h2>
+            <h2 className="text-xl font-semibold text-foreground">{user}</h2>
             <p className="text-muted-foreground">+94 75 956 3850</p>
           </div>
             <ul className="space-y-2" style={{marginTop:'10%'}} >
-              <li className="flex items-center py-2 px-3 text-black rounded-lg hover:bg-gray-100 " style={{cursor:'pointer'}}>
-                <span>Add to favorites</span>
+              <li className="flex items-center py-2 px-3 text-black rounded-lg hover:bg-gray-100 " onClick={toggleFavorite} style={{cursor:'pointer'}}>
+                <i className={`bi ${isFavorite ? 'bi-star' : 'bi-star-fill'}`}></i>&nbsp;&nbsp;
+                <span>{isFavorite ? 'Remove from favorites' : 'Add to favorites'}</span>        
               </li>
               <li className="flex items-center py-2 px-3 text-destructive rounded-lg hover:bg-gray-100 "  style={{cursor:'pointer'}}>
-                <span>Block My Airtel</span>
+                <i className="bi bi-ban"></i>&nbsp;&nbsp;
+                <span>Block {user}</span>
               </li>
               <li className="flex items-center py-2 px-3 text-destructive rounded-lg hover:bg-gray-100 "  style={{cursor:'pointer'}}>
-                <span>Report My Airtel</span>
+                <i className="bi bi-hand-thumbs-down"></i>&nbsp;&nbsp;
+                <span>Unfriend {user}</span>
               </li>
               <li className="flex items-center py-2 px-3 text-destructive rounded-lg hover:bg-gray-100 "  style={{cursor:'pointer'}}>
+                <i className="bi bi-trash"></i>&nbsp;&nbsp;
                 <span>Delete chat</span>
               </li>
             </ul>
