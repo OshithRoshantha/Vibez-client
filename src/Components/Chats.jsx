e091e452c77e5d093e7cbed57832562be406b88e
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-26 18:21:00 +0530

    style: update chat list component for dark mode compatibility

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -13,14 +13,14 @@ export default function Chats({showDirectMessages, darkMode}) {
                     <button className={`${darkMode ? 'bg-[#223b51] text-[#59abff] hover:bg-[#184e88]':'bg-gray-300 text-gray-600  hover:bg-gray-200'} px-4 py-2 rounded-full border-none`}>Favorites</button>
                 </div>
                 <div className='chat-list'>
-                <div onClick={showDirectMessages}  className="space-y-2" style={{cursor: 'pointer'}}>
-                    <div className="flex items-center p-2 hover:bg-muted rounded">
+                <div onClick={showDirectMessages} className="space-y-2" style={{cursor: 'pointer'}}>
+                    <div className={`${darkMode ? 'hover:bg-[#2d3243]' : 'hover:bg-muted'} flex items-center p-2 rounded`}>
                         <img src="https://placehold.co/40x40" alt="User" className="rounded-full mr-2 w-18 h-18" />
                         <div>
-                            <div className="font-medium">FriendName</div>
-                            <div className="text-sm text-muted-foreground">User1: Lorem ipsum dolor sit amet.</div>
+                            <div className={`${darkMode ? 'text-white':''} font-medium`}>FriendName</div>
+                            <div className={`${darkMode ? 'text-gray-400':'text-muted-foreground'} text-sm `}>User1: Lorem ipsum dolor sit amet.</div>
                         </div>
-                        <span className="ml-auto text-xs">13:14</span>
+                        <span className={`${darkMode ? 'text-gray-400':''} ml-auto text-xs`}>13:14</span>
                     </div>
                 </div>
                 </div>
