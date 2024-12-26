7bb6dd5fe796675fa8f35aeed8d29313c4219c8e
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-26 09:59:13 +0530

    style: add confirmation popups for deleting groups and chats in GroupMemberList, GroupMemberList2, and Settings components

--- src/Components/GlobalAlert.jsx
+++ src/Components/GlobalAlert.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function GlobalAlert({text,textOP,button1,button2,btn1Function,btn2Function}) {
   return (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50" style={{zIndex: '100'}}>
         <div className="bg-white p-6 rounded-lg shadow-lg text-left ">
             <h2 className="text-lg font-semibold text-black mb-1">
             {text}
