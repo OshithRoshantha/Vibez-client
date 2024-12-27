211d96794534457d6b5b79421ffdf04308ebd1c8
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-27 10:45:27 +0530

    style: add dark mode support to GroupMemberList2 and update GroupInfo component

--- src/Components/GroupMemberList2.jsx
+++ src/Components/GroupMemberList2.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react'
 import GlobalAlert from './GlobalAlert';
 
-export default function GroupMemberList2() {
+export default function GroupMemberList2({darkMode}) {
   const [exitGroupPopup, setExitGroupPopup] = useState(false);
 
   function toggleExitGroupPopup(){
@@ -12,24 +12,24 @@ export default function GroupMemberList2() {
 
   return (
     <div className="mt-0">
-        {exitGroupPopup && <GlobalAlert text={`Exit "${groupName}" group?`} textOP={''} button1={'Cancel'} button2={'Exit group'} btn1Function={toggleExitGroupPopup} btn2Function={toggleExitGroupPopup}/>}
+        {exitGroupPopup && <GlobalAlert darkMode={darkMode} text={`Exit "${groupName}" group?`} textOP={''} button1={'Cancel'} button2={'Exit group'} btn1Function={toggleExitGroupPopup} btn2Function={toggleExitGroupPopup}/>}
         <div className="flex items-center mb-2">
 
         <div className="w-8 h-8 rounded-full bg-green-500 mr-2"></div>
         <div>
-            <span className="font-semibold">You</span>
-            <p className="text-sm text-muted-foreground">Hey there! I am using Vibez.</p>
+            <span className={`${darkMode ? 'text-white':''} font-semibold`}>You</span>
+            <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'} text-sm `}>Hey there! I am using Vibez.</p>
         </div>
-        <button onClick={toggleExitGroupPopup} className="ml-auto text-sm bg-red-300 text-white hover:bg-red-200 border-none">Exit group</button>
+        <button onClick={toggleExitGroupPopup} className="ml-auto text-sm bg-red-400 text-white hover:bg-red-300 border-none">Exit group</button>
         </div>
 
         <div className="flex items-center mb-2">
         <div className="w-8 h-8 rounded-full bg-blue-500 mr-2"></div>
         <div>
-            <span className="font-semibold">user01</span>
-            <p className="text-sm text-muted-foreground">Available</p>
+            <span className={`${darkMode ? 'text-white':''} font-semibold`}>user01</span>
+            <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'} text-sm `}>Available</p>
         </div>
-        <p className="ml-auto text-sm text-muted-foreground">Owner</p>
+        <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'} text-sm ml-auto`}>Owner</p>
         </div>
     </div>
   )
