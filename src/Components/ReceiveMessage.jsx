906ddfff42d4ba953b05b06322d6e1d9e168248b
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-27 11:54:36 +0530

    style: update background color for dark mode in ReceiveMessage component

--- src/Components/ReceiveMessage.jsx
+++ src/Components/ReceiveMessage.jsx
@@ -4,7 +4,7 @@ export default function ReceiveMessage({message, time}) {
   return (
     <div className="w-full" style={{display:'flex', justifyContent:'left'}}>
         <div className="flex items-start mb-2">
-        <div className="bg-[#262729] text-white p-2 rounded-lg max-w-xs break-words">
+        <div className="bg-[#1c1c1c] text-white p-2 rounded-lg max-w-xs break-words">
             <p style={{fontSize:'95%'}}>{message}</p>
             <span className="text-xs text-muted-foreground">{time}</span>
         </div>
