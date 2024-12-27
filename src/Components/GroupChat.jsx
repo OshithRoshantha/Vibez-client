be3f16eb5957992d73b31dc0ac22d3688db34a80
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-27 12:08:11 +0530

    style: add dark mode support to GroupChat, GroupInfo, GroupReceiveMessage, and GroupSendMessage components

--- src/Components/GroupChat.jsx
+++ src/Components/GroupChat.jsx
@@ -2,29 +2,32 @@ import GroupReceiveMessage from "./GroupReceiveMessage";
 import GroupSendMessage from "./GroupSendMessage";
 
 
-export default function GroupChat({showGroupInfoMenu}) {
+export default function GroupChat({showGroupInfoMenu, darkMode}) {
+
+  const chatWallpaper = darkMode ? 'url(./src/assets/Wallpapers/dark.png)' : 'url(./src/assets/Wallpapers/light.png)';
+
   return (
     <div>
-        <div className="bg-background min-h-screen flex flex-col">
-        <div onClick={showGroupInfoMenu} style={{cursor:'pointer'}} className="flex items-center px-4 py-3 border-b border-border">
+        <div className={`${darkMode ? 'bg-[#262729]' : 'bg-background' } min-h-screen flex flex-col`}>
+        <div onClick={showGroupInfoMenu} style={{cursor:'pointer'}} className={`${darkMode ? 'border-gray-600' : 'border-border'} flex items-center px-4 py-3 border-b`}>
             <div className="flex items-center">
             <img src="https://placehold.co/40x40" alt="User Avatar" className="rounded-full mr-2" />
             <div>
-              <span className="text-lg font-semibold text-black">Group1</span>
-              <p className="text-muted-foreground mt-0" style={{fontSize:'70%'}}>Click here for group info</p>
+              <span className={`${darkMode ? 'text-white':'text-black'} text-lg font-semibold`}>Group1</span>
+              <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'} mt-0`} style={{fontSize:'70%'}}>Click here for group info</p>
             </div>
             </div>
         </div>
-        <div className="p-4" style={{height:'78vh', overflowY:'auto', scrollbarWidth:'none'}}>
+        <div className="p-4" style={{height:'78vh', overflowY:'auto', scrollbarWidth:'none', backgroundImage: chatWallpaper, backgroundSize: 'cover' }}>
             <GroupReceiveMessage senderName={'sendUser 01'} time={'00:26'} message={'Simple AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
             <GroupReceiveMessage senderName={'sendUser 02'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAA'}/>
             <GroupReceiveMessage senderName={'sendUser 01'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
             <GroupSendMessage time={'00:26'} message={'send message'}/>
             <GroupReceiveMessage senderName={'sendUser 01'} time={'00:26'} message={'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'}/>
         </div>
-        <div className=" px-4 py-3  border-t border-border bg-card" style={{display:'flex', alignItems:'center',columnGap:'1rem'}}>
-            <input type="text" placeholder="Type a message" className="w-full p-2 rounded-lg bg-input text-black" />
-            <span><i style={{cursor:'pointer'}} className="bi bi-send-fill text-2xl"></i></span>
+        <div className={`${darkMode ? 'border-gray-600 bg-[#262729]' : 'border-border bg-card'} px-4 py-3  border-t`} style={{display:'flex', alignItems:'center',columnGap:'1rem'}}>
+            <input type="text" placeholder="Type a message" className={`${darkMode ? ' text-white' : 'bg-input text-black'} focus:border-none focus:outline-none w-full p-2 rounded-lg`} />
+            <span><i style={{cursor:'pointer'}} className="bi bi-send-fill text-2xl text-primary"></i></span>
         </div>
         </div>
     </div>
