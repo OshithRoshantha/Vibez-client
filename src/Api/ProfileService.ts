3d08fb71abb866dfa48434069433a555312199ea
Oshith Roshantha <oedirisuriya@gmail.com>
2024-12-31 13:19:52 +0530

    fix: correct API endpoint in createAccount function and streamline handleSignUp logic in SignupElement

--- src/Api/ProfileService.ts
+++ src/Api/ProfileService.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const createAccount = async (email: string, name: string, password: string) => {
-    const response = await axios.post('http://localhost:8080/vibez//register', {
+    const response = await axios.post('http://localhost:8080/vibez/register', {
         email: email,
         userName: name,
         password: password
