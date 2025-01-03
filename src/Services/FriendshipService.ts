0ff30c7200b23f686d96c0280725817fe9173bde
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-03 22:15:57 +0530

    feat: add functionality to approve friend requests and update friendship status in SearchResult component

--- src/Services/FriendshipService.ts
+++ src/Services/FriendshipService.ts
@@ -26,6 +26,27 @@ export const sendFriendRequest = async (friendId: string): Promise<void> => {
     });
 };
 
+export const acceptFriendRequest = async (friendshipId: string): Promise<void> => {
+    return new Promise((resolve) => {
+        const token = sessionStorage.getItem('token');
+        const status = 'ACCEPTED';
+
+        const socket = new WebSocket(`ws://localhost:8080/vibez-websocket?token=${token}`);
+
+        socket.onopen = () => {
+            const message = {
+                action: 'friendshipService',
+                body: {
+                    friendshipId,
+                    status
+                },
+            };
+        socket.send(JSON.stringify(message));
+        resolve(); 
+        };
+    });
+};
+
 export const getFriendshipStatus = async (friendshipId: string) => {
     const userId = sessionStorage.getItem('userId');
     const response = await axios.get(`http://localhost:8080/vibez/friends/friendshipInfo/${friendshipId}`, {
