b960b2f020cfcef2df9e08588335af90ccc5ff78
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-03 12:27:32 +0530

    feat: implement friend request functionality and update friendship status handling in SearchResult component

--- src/Components/SearchResult.jsx
+++ src/Components/SearchResult.jsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { sendFriendRequest, getFriendshipStatus, getFriendshipId } from '../Services/FriendshipService'
 
 export default function SearchResult({darkMode, profileName, profileAbout, profileImage, profileId}) {
+
+  const [friendshipStatus, setFriendshipStatus] = useState('');
+  const [friendshipId, setFriendshipId] = useState('');
+  
+  const addFriend = async () => {
+    await sendFriendRequest(profileId);
+    friendshipInfo();
+    retriveFriendshipId();
+  }
+
+  const friendshipInfo = async () => {
+    const status = await getFriendshipStatus(profileId);
+    setFriendshipStatus(status);
+  }
+
+  const retriveFriendshipId = async () => {
+    const id = await getFriendshipId(profileId);
+    setFriendshipId(id);
+  }
+
+  useEffect(() => {
+    friendshipInfo();
+  }, []);
+
   return (
     <div className="flex items-center justify-between border-border ">
     <div className="flex items-center">
@@ -11,7 +36,36 @@ export default function SearchResult({darkMode, profileName, profileAbout, profi
         </div>
     </div>
     <div className='btn-container'>
-        <button className="border-none hover:border-none bg-primary text-white p-2 px-3 rounded">Add friend</button>
+        {friendshipStatus === '' && (
+          <button
+            onClick={addFriend}
+            className="border-none hover:border-none bg-primary text-white p-2 px-3 rounded"
+          >
+            Add Friend
+          </button>
+        )}
+        {friendshipStatus === 'ACCEPTED' && (
+          <button
+            className="border-none hover:border-none bg-primary text-white p-2 px-3 rounded"
+          >
+            Message
+          </button>
+        )}
+        {friendshipStatus === 'REQUESTED' && (
+          <button
+            disabled
+            className="border-none bg-blue-400 text-white p-2 px-3 rounded cursor-not-allowed"
+          >
+            Requested
+          </button>
+        )}
+        {friendshipStatus === 'CONFIRM' && (
+          <button
+            className="border-none hover:border-none bg-primary text-white p-2 px-3 rounded"
+          >
+            Confirm
+          </button>
+        )}    
     </div>
     </div>
   )
