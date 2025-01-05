9b790d64d3b03ef418b66d73d95d51b9c527854c
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-05 21:13:01 +0530

    fix: update PreiviewAcceptedRequests component to use setBlockPopup and setUnfriendPopup for improved functionality

--- src/Components/PreiviewAcceptedRequests.jsx
+++ src/Components/PreiviewAcceptedRequests.jsx
@@ -6,7 +6,7 @@ import {
 import { unFriend } from '../Services/FriendshipService';
 import { useState } from 'react';
 
-export default function PreiviewAcceptedRequests({darkMode, friendshipId, profileId, profileName, profilePicture, status, profileAbout, toggleBlockPopup, toggleUnfriendPopup}) {
+export default function PreiviewAcceptedRequests({darkMode, friendshipId, profileId, profileName, profilePicture, status, profileAbout, setBlockPopup, setUnfriendPopup}) {
   
     const [isUnfriended, setIsUnfriended] = useState(false);
 
@@ -15,95 +15,95 @@ export default function PreiviewAcceptedRequests({darkMode, friendshipId, profil
         setIsUnfriended(true);
     }
     return (
-<>
-{!isUnfriended && <>
-        <div className="flex items-center justify-between border-border py-0 mt-2">
-        <div className="flex items-center">
-            <img src={profilePicture} className="rounded-full mr-2" style={{ height: '45px' }} />
-            <div>
-            <p className={`${darkMode ? 'text-white' : ''} font-medium`}>{profileName}</p>
-            <p className={`${darkMode ? 'text-gray-400' : 'text-muted-foreground'} text-sm`}>{profileAbout}</p>
-            </div>
-        </div>
+            <>
+            {!isUnfriended && <>
+                    <div className="flex items-center justify-between border-border py-0 mt-2">
+                    <div className="flex items-center">
+                        <img src={profilePicture} className="rounded-full mr-2" style={{ height: '45px' }} />
+                        <div>
+                        <p className={`${darkMode ? 'text-white' : ''} font-medium`}>{profileName}</p>
+                        <p className={`${darkMode ? 'text-gray-400' : 'text-muted-foreground'} text-sm`}>{profileAbout}</p>
+                        </div>
+                    </div>
 
-        <div className="btn-container">
-            <div className="ml-mr-4 btns">
-            <i className="bi bi-chat-fill text-primary"></i>
-            </div>
+                    <div className="btn-container">
+                        <div className="ml-mr-4 btns">
+                        <i className="bi bi-chat-fill text-primary"></i>
+                        </div>
 
-            <Popover>
-            <PopoverTrigger asChild>
-                <div className="btns">
-                <i className={`${darkMode ? 'text-white' : ''} bi bi-three-dots-vertical`}></i>
-                </div>
-            </PopoverTrigger>
+                        <Popover>
+                        <PopoverTrigger asChild>
+                            <div className="btns">
+                            <i className={`${darkMode ? 'text-white' : ''} bi bi-three-dots-vertical`}></i>
+                            </div>
+                        </PopoverTrigger>
 
-            <PopoverContent
-                style={{
-                width: '220px',
-                marginRight: '200px',
-                height: '105px',
-                backgroundColor: darkMode ? '#262729' : '',
-                
-                }}
-            >
-                <div className="bg-card text-card-foreground p-0 rounded-lg pl-3">
-                <div
-                    className="flex-grow friend-buttons"
-                    style={{
-                    marginLeft: '-20px',
-                    marginTop: '-17px',
-                    backgroundColor: darkMode ? '#262729' : '',
-                    }}
-                >
-                    <button
-                    onClick={toggleBlockPopup}
-                    className={`${darkMode ? 'text-white' : ''} flex flex-grow items-center w-full p-2 text-left rounded bg-transparent border-none focus:ring-0 hover:border-none`}
-                    >
-                    <span
-                        className="material-icons mr-3"
-                        style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: darkMode ? '#3b3c3e' : '#d1d1d1',
-                        width: '29px',
-                        height: '29px',
-                        borderRadius: '50%',
-                        }}
-                    >
-                        <i className="bi bi-slash-circle-fill "></i>
-                    </span>	
-                    Block {profileName.split(' ')[0]}
-                    </button>
+                        <PopoverContent
+                            style={{
+                            width: '220px',
+                            marginRight: '200px',
+                            height: '105px',
+                            backgroundColor: darkMode ? '#262729' : '',
+                            
+                            }}
+                        >
+                            <div className="bg-card text-card-foreground p-0 rounded-lg pl-3">
+                            <div
+                                className="flex-grow friend-buttons"
+                                style={{
+                                marginLeft: '-20px',
+                                marginTop: '-17px',
+                                backgroundColor: darkMode ? '#262729' : '',
+                                }}
+                            >
+                                <button
+                                onClick={setBlockPopup}
+                                className={`${darkMode ? 'text-white' : ''} flex flex-grow items-center w-full p-2 text-left rounded bg-transparent border-none focus:ring-0 hover:border-none`}
+                                >
+                                <span
+                                    className="material-icons mr-3"
+                                    style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    backgroundColor: darkMode ? '#3b3c3e' : '#d1d1d1',
+                                    width: '29px',
+                                    height: '29px',
+                                    borderRadius: '50%',
+                                    }}
+                                >
+                                    <i className="bi bi-slash-circle-fill "></i>
+                                </span>	
+                                Block {profileName.split(' ')[0]}
+                                </button>
 
-                    <button
-                    onClick={toggleUnfriendPopup}
-                    className={`${darkMode ? 'text-white' : ''} flex flex-grow items-center w-full p-2 text-left rounded bg-transparent border-none focus:ring-0 hover:border-none`}
-                    >
-                    <span
-                        className="material-icons mr-3"
-                        style={{
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        backgroundColor: darkMode ? '#3b3c3e' : '#d1d1d1',
-                        width: '29px',
-                        height: '29px',
-                        borderRadius: '50%',
-                        }}
-                    >
-                        <i className="bi bi-person-x-fill "></i>
-                    </span>
-                    Remove {profileName.split(' ')[0]}
-                    </button>
-                </div>
-                </div>
-            </PopoverContent>
-            </Popover>
-        </div>
-        </div>
-    </>}
-</>
+                                <button
+                                onClick={setUnfriendPopup}
+                                className={`${darkMode ? 'text-white' : ''} flex flex-grow items-center w-full p-2 text-left rounded bg-transparent border-none focus:ring-0 hover:border-none`}
+                                >
+                                <span
+                                    className="material-icons mr-3"
+                                    style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    backgroundColor: darkMode ? '#3b3c3e' : '#d1d1d1',
+                                    width: '29px',
+                                    height: '29px',
+                                    borderRadius: '50%',
+                                    }}
+                                >
+                                    <i className="bi bi-person-x-fill "></i>
+                                </span>
+                                Remove {profileName.split(' ')[0]}
+                                </button>
+                            </div>
+                            </div>
+                        </PopoverContent>
+                        </Popover>
+                    </div>
+                    </div>
+                </>}
+            </>
   )
 }
