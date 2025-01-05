3ab47e4fb27a73f378930c1437c8d22131757035
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-05 19:48:03 +0530

    feat: implement unfriend functionality and update friendship handling in Friends component

--- src/Services/FriendshipService.ts
+++ src/Services/FriendshipService.ts
@@ -47,6 +47,27 @@ export const acceptFriendRequest = async (friendshipId: string): Promise<void> =
     });
 };
 
+export const unFriend = async (friendshipId: string): Promise<void> => {
+    return new Promise((resolve) => {
+        const token = sessionStorage.getItem('token');
+        const status = 'UNFRIENDED';
+
+        const socket = new WebSocket(`ws://localhost:8080/vibez-websocket?token=${token}`);
+
+        socket.onopen = () => {
+            const message = {
+                action: 'friendshipService',
+                body: {
+                    friendshipId,
+                    status
+                },
+            };
+        socket.send(JSON.stringify(message));
+        resolve(); 
+        };
+    });
+}
+
 export const getFriendshipStatus = async (friendshipId: string) => {
     const response = await axios.get(`http://localhost:8080/vibez/friends/getStatus/${friendshipId}`, {
         headers: {
