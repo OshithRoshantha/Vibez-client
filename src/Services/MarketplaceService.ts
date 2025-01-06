bc188658c3843e20dc4a945f81e592f2c662c21d
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-06 21:35:27 +0530

    feat: implement marketplace item fetching and display in Marketplace component

---
+++ src/Services/MarketplaceService.ts
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export const getMarketplaceItems  = async () => {
+    const userId = sessionStorage.getItem('userId');
+    const response = await axios.get(`http://localhost:8080/vibez/product/findAll/${userId}`, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+        },
+    });
+    return response.data;
+};
+
