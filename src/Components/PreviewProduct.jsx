dafeda76559f0a459a1a916ff6a48f00e6ee5f94
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-07 13:12:26 +0530

    feat: add active listing count retrieval in Marketplace component

--- src/Components/PreviewProduct.jsx
+++ src/Components/PreviewProduct.jsx
@@ -11,7 +11,7 @@ export default function PreviewProduct({darkMode, showProductInfo, productImages
     <>
     <div className="bg-card rounded-lg shadow-md overflow-hidden" style={{height:'80%', cursor:'pointer',backgroundColor: darkMode ? '#56585a':''}} onClick={handleClick}>
         <img src={productImages[0]} alt="Se2" className="w-full h-30 object-cover" style={{height:'65%'}}/>
-        <div className="pl-4 pr-4 pt-2 pb-2 mt-3">
+        <div className="pl-4 pr-4 pt-2 pb-2 mt-0">
             <h2 className={`${darkMode ? 'text-white':''} text-lg font-semibold`}>LRK {productPrice}</h2>
             <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'}`}>{productTitle}</p>
         </div>
--- src/Services/MarketplaceService.ts
+++ src/Services/MarketplaceService.ts
@@ -19,6 +19,16 @@ export const getItemInfo = async (productId: string) => {
     return response.data;
 };
 
+export const getActiveListingCount = async () => {
+    const userId = sessionStorage.getItem('userId');
+    const response = await axios.get(`http://localhost:8080/vibez/product/count/${userId}`, {
+        headers: {
+            Authorization: `Bearer ${sessionStorage.getItem('token')}`,
+        },
+    });
+    return response.data;
+}
+
 export const addListing = async (productTitle: string, price: string, productDesc: string, productPhotos: string[], location: string, visibleToFriends: boolean, condition: string): Promise<void> => {
     return new Promise((resolve) => {
         const token = sessionStorage.getItem('token');
