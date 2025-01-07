e686c3b15416693f9d60558e90b27a95d5d567e5
sahannimesh22 <sahannimesh22@gmail.com>
2025-01-07 20:04:29 +0530

    extract gradient overlay into a reusable component

--- src/Components/Landinganimation2.jsx
+++ src/Components/Landinganimation2.jsx
@@ -6,3 +6,15 @@ ReviewCard.propTypes = {
   username: PropTypes.string.isRequired,
   body: PropTypes.string.isRequired,
 };
+const GradientOverlay = ({ position }) => (
+    <div
+      className={`pointer-events-none absolute inset-y-0 ${position} w-1/3 bg-gradient-to-${
+        position === "left" ? "r" : "l"
+      } from-white dark:from-background`}
+    ></div>
+  );
+  
+  GradientOverlay.propTypes = {
+    position: PropTypes.oneOf(['left', 'right']).isRequired,
+  };
+  
