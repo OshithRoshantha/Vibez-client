ffb2196f6d3d2006128dc83081c1889850c2537b
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-07 12:48:36 +0530

    fix: adjust height and margin in PreviewProduct component for better layout

--- src/Components/PreviewProduct.jsx
+++ src/Components/PreviewProduct.jsx
@@ -9,10 +9,10 @@ export default function PreviewProduct({darkMode, showProductInfo, productImages
 
   return (
     <>
-    <div className="bg-card rounded-lg shadow-md overflow-hidden" style={{height:'87%', cursor:'pointer',backgroundColor: darkMode ? '#56585a':''}} onClick={handleClick}>
+    <div className="bg-card rounded-lg shadow-md overflow-hidden" style={{height:'80%', cursor:'pointer',backgroundColor: darkMode ? '#56585a':''}} onClick={handleClick}>
         <img src={productImages[0]} alt="Se2" className="w-full h-30 object-cover" style={{height:'65%'}}/>
-        <div className="pl-4 pr-4 pt-2 pb-2">
-            <h2 className={`${darkMode ? 'text-white':''} text-lg font-semibold`}>{productPrice}</h2>
+        <div className="pl-4 pr-4 pt-2 pb-2 mt-3">
+            <h2 className={`${darkMode ? 'text-white':''} text-lg font-semibold`}>LRK {productPrice}</h2>
             <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'}`}>{productTitle}</p>
         </div>
     </div>
