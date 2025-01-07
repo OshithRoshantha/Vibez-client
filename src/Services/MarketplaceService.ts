a5933337b8cdf87d2af5bc9c447852854dd0fc72
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-07 12:37:26 +0530

    feat: implement listing creation with validation in Marketplace component

--- src/Services/MarketplaceService.ts
+++ src/Services/MarketplaceService.ts
@@ -18,3 +18,32 @@ export const getItemInfo = async (productId: string) => {
     });
     return response.data;
 };
+
+export const addListing = async (productTitle: string, price: string, productDesc: string, productPhotos: string[], location: string, visibleToFriends: boolean, condition: string): Promise<void> => {
+    return new Promise((resolve) => {
+        const token = sessionStorage.getItem('token');
+        const sellerId = sessionStorage.getItem('userId');
+        const productAction = 'ADD';
+
+        const socket = new WebSocket(`ws://localhost:8080/vibez-websocket?token=${token}`);
+
+        socket.onopen = () => {
+            const message = {
+                action: 'marketplaceService',
+                body: {
+                    sellerId,
+                    productTitle,
+                    productDesc,
+                    condition,
+                    price,
+                    location,
+                    productPhotos,
+                    visibleToFriends,
+                    productAction
+                },
+            };
+        socket.send(JSON.stringify(message));
+        resolve(); 
+        };
+    });
+}
