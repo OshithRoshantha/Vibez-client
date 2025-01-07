655520556747b45b3dceb2db8c6b3d1f69eb80ff
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-07 14:43:50 +0530

    feat: add functionality to fetch and display user's listings in Marketplace component

--- src/Components/YourListings.jsx
+++ src/Components/YourListings.jsx
@@ -1,7 +1,7 @@
 import './Styles/Column2.css'
 import { useState} from 'react';
 
-export default function YourListings({showEditListingMenu, darkMode}) {
+export default function YourListings({showEditListingMenu, darkMode, productTitle, productDesc, productPhotos, price}) {
   const [deleteMenu, setDeleteMenu] = useState(false);
 
   function showDeleteMenu() {
@@ -22,8 +22,8 @@ export default function YourListings({showEditListingMenu, darkMode}) {
                     <p className={`${darkMode ? 'text-gray-300' : 'text-muted-foreground'} mt-2`}>Are you sure that you want to delete this listing?</p>
                 </div>
                 <div style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-                  <p className={`${darkMode ? 'text-gray-200' : 'text-muted-foreground'} mt-2`}>LKR57,500</p>
-                  <p className={`${darkMode ? 'text-gray-200' : 'text-muted-foreground'}`}>🛒 Phones</p>
+                  <p className={`${darkMode ? 'text-gray-200' : 'text-muted-foreground'} mt-2`}>LKR {price}</p>
+                  <p className={`${darkMode ? 'text-gray-200' : 'text-muted-foreground'}`}>🛒 {productTitle}</p>
                 </div>
                 <div>
                   <button style={{cursor:'pointer'}} onClick={hideDeleteMenu} className="bg-primary text-primary-foreground px-4 py-1 rounded-lg w-full mt-3 mb-2">Delete</button>
@@ -33,11 +33,11 @@ export default function YourListings({showEditListingMenu, darkMode}) {
             </div>
           </div>}
         <div className="w-full bg-card rounded-lg py-2" style={{display:'flex',columnGap:'5%',justifyContent:'center',alignItems:'center', backgroundColor: darkMode ? '#262729' : ''}}>
-        <img src="https://placehold.co/300x200" alt="9-cube shelf" className="your-listning-img" />
+        <img src={productPhotos} alt="product-image" className="your-listning-img" />
         <div>
-            <h2 className={`${darkMode ? 'text-white':''} text-sm font-semibold mt-2`}>9-cube shelf</h2>
-            <p className="text-lg font-bold text-primary">$10</p>
-            <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'} text-sm`}>Listed on 6/19</p>
+            <h2 className={`${darkMode ? 'text-white':''} text-sm font-semibold mt-2`}>{productTitle}</h2>
+            <p className="text-lg font-bold text-primary">LRK {price}</p>
+            <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'} text-sm`}>{productDesc}</p>
             <div className="flex space-x-2 mt-1">
                 <button onClick={showEditListingMenu} className="bg-primary text-white hover:bg-secondary/80 px-2 py-1 rounded-lg text-sm"><i className="bi bi-pencil-square"></i>&nbsp;Edit listing</button>
                 <button onClick={showDeleteMenu} className={`${darkMode ? 'bg-[#6a6b6d] text-white hover:bg-[#545454]':'bg-muted text-muted-foreground hover:bg-gray-300'}  px-2 py-1 text-sm border-none`}><i className="bi bi-trash"></i>&nbsp;Delete listing</button>
