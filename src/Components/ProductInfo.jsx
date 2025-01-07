d47d61dcf3e6cd192d3705849c9de7b8e0843c96
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-07 13:29:11 +0530

    fix: update loading state and replace itemInfo with static product details in ProductInfo component

--- src/Components/ProductInfo.jsx
+++ src/Components/ProductInfo.jsx
@@ -4,11 +4,22 @@ import { useState, useEffect } from 'react';
 
 export default function ProductInfo({productId, darkMode}) {
 
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
     
     const productPrice = 'LKR 57,500';
     const productDescription = 'This is a brand new Apple iPhone 11 Pro. It is in perfect condition and has never been used. It comes with a charger and a case.';
     const sellerName = 'John Doe';
+
+    const fetchItemInfo = async () => {
+        setIsLoading(true);
+        const response = await getItemInfo(productId);
+        console.log('This is response' + response);
+        setIsLoading(false);
+    }
+
+    useEffect(() => {
+        fetchItemInfo();
+    }, []);
 
   return (
     <div>
@@ -19,12 +30,12 @@ export default function ProductInfo({productId, darkMode}) {
                     <img src="https://placehold.co/400x300" alt="Apple 11 Pro" className="rounded-lg mb-0"/>
                 </div>
                 <h2 className={`${darkMode ? 'text-white':''} text-lg font-bold`}>{productId}</h2>
-                <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'}`}>{itemInfo.price}</p>
+                <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'}`}>{productPrice}</p>
                 <p className={`${darkMode ? 'text-white':''} text-sm`}>Send seller a message</p>
                 <button className="bg-primary text-white  py-2 px-4 rounded-lg mt-2 mr-5">Send Message</button>
                 <button className={`${darkMode ? 'bg-[#6a6b6d] text-white hover:bg-[#545454]':'bg-muted text-muted-foreground hover:bg-gray-300'} py-2 px-4 rounded-lg border-none`}>Share offer</button>
                 <h3 className={`${darkMode ? 'text-white':''} mt-4 font-semibold`}>Description</h3>
-                <p className={`${darkMode ? 'text-gray-300':'text-muted-foreground'} text-sm`}>{itemInfo.productDesc}</p>
+                <p className={`${darkMode ? 'text-gray-300':'text-muted-foreground'} text-sm`}>{productDescription}</p>
                 <div className="mt-4">
                     <h4 className={`${darkMode ? 'text-white':''} font-semibold`}>Seller information</h4>
                     <div className="flex items-center mt-2">
