6827240a298cd70f3c19ead576d5624759d09fc8
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-20 12:32:51 +0530

    feat: format product prices using Intl.NumberFormat for better readability

--- src/Components/PreviewProduct.jsx
+++ src/Components/PreviewProduct.jsx
@@ -9,10 +9,10 @@ export default function PreviewProduct({darkMode, showProductInfo, productImages
 
   return (
     <>
-    <div className="bg-card rounded-lg shadow-md overflow-hidden" style={{height:'80%', cursor:'pointer',backgroundColor: darkMode ? '#56585a':''}} onClick={handleClick}>
-        <img src={productImages[0]} alt="Se2" className="w-full h-30 object-cover" style={{height:'65%'}}/>
+    <div className="bg-card rounded-lg shadow-md overflow-hidden" style={{height:'100%', cursor:'pointer',backgroundColor: darkMode ? '#56585a':''}} onClick={handleClick}>
+        <img src={productImages[0]} alt="Se2" className="w-full h-30 object-cover" style={{height:'150px'}}/>
         <div className="pl-4 pr-4 pt-2 pb-2 mt-0">
-            <h2 className={`${darkMode ? 'text-white':''} text-lg font-semibold`}>LRK {productPrice}</h2>
+            <h2 className={`${darkMode ? 'text-white':''} text-lg font-semibold`}>LRK {new Intl.NumberFormat().format(productPrice)}</h2>
             <p className={`${darkMode ? 'text-gray-400':'text-muted-foreground'}`}>{productTitle}</p>
         </div>
     </div>
