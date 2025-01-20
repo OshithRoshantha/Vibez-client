68f8b4d3fff7005152d85fe08d0c0797c2f11654
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-20 11:10:54 +0530

    feat: replace img tags with divs for user and group avatar display

--- src/Components/GroupChat.jsx
+++ src/Components/GroupChat.jsx
@@ -201,7 +201,7 @@ export default function GroupChat({ showGroupInfoMenu, darkMode, groupId, fetchU
             ) : (
               <div>
                 <div className="flex items-center">
-                  <img src={groupAvatar} alt="User Avatar" className="rounded-full mr-2" style={{height:'45px', width:'45px'}}/>
+                  <div className="rounded-full mr-2" style={{ height: '45px', width: '45px', background: `center / cover no-repeat url(${groupAvatar})` }}></div>
                   <div>
                     <span className={`${darkMode ? 'text-white' : 'text-black'} text-lg font-semibold`}>{groupName}</span>
                     <p className={`${darkMode ? 'text-gray-400' : 'text-muted-foreground'} mt-0`} style={{ fontSize: '70%' }}>Click here for group info</p>
