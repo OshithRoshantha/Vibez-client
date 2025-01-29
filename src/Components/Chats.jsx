8fbca9ffea69c50d9f5387d6568f6e1b1f1a40b3
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-29 16:18:28 +0530

    feat: refine account deletion handling to trigger chat fetch only for direct chats

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -90,7 +90,7 @@ export default function Chats({showDirectMessages, darkMode, setReceiverId}) {
                     }
                 }
 
-                if(lastMessage.action === 'accountDelete'){
+                if(lastMessage.action === 'accountDelete' && lastMessage.typeOfAction === 'directChat'){
                     fetchAllChats();
                     fetchFavouriteChats();
                 }
