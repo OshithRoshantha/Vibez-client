4a21e6f199651e1bde90afb3f46efaf223117bbb
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-30 09:59:57 +0530

    feat: enhance Signup component layout for mobile responsiveness

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -2,8 +2,11 @@ import './Styles/Signup.css'
 import SignupElement from '@/Components/SignupElement'
 import mainLogo from '../assets/Icons/main-logo.png'
 import { useNavigate } from 'react-router-dom';
+import { useIsMobile } from '../hooks/useIsMobile';
 
 export default function Signup() {
+
+  const isMobile = useIsMobile();
   const navigate = useNavigate();
   function navSignin() {
       navigate('/');
@@ -11,17 +14,19 @@ export default function Signup() {
 
   return (
     <div className='second-container'>
-      <div className='header'>
+      <div className='header' style={{width: isMobile ? '80%' : ''}}>
         <div className='header-right'>
           <p className="h1 signup-main">Sign Up</p>
           <p className="display-6 signup-sub">Let's get started, Sign up and keep the conversation flowing.</p><br></br>
           <p>Already have an account? <a className='signup-link' onClick={() => {navSignin();}}>Sign In</a></p>
         </div>
+        {!isMobile && <>
         <div className='logo'>
           <img src={mainLogo} className='main-logo-signup' alt='Main Logo'/>
         </div>
+        </>}
       </div>
-      <div className='signup-container'>
+      <div className='signup-container' style={{backgroundColor: isMobile ? '#F5F5F5' : '', width: isMobile ? '80%' : ''}}>
         <SignupElement/>
       </div>
     </div>
