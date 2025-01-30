3da808104576334b6e1f1725d494641f7a1008a6
Oshith Roshantha <oedirisuriya@gmail.com>
2025-01-30 14:26:56 +0530

    feat: adjust component heights for improved mobile responsiveness

--- src/Components/Marketplace.jsx
+++ src/Components/Marketplace.jsx
@@ -297,7 +297,7 @@ export default function Marketplace({darkMode, showDirectMessages, setReceiverId
 
   return (
     <div>
-        <div className={`${darkMode ? 'border-gray-600 border-r border-border':'border-r border-border'}  p-4 chats-column`} style={{backgroundColor: darkMode ? '#262729' : '', height:'100vh', width: isMobile ? '100vw' : ''}}>
+        <div className={`${darkMode ? 'border-gray-600 border-r border-border':'border-r border-border'}  p-4 chats-column`} style={{backgroundColor: darkMode ? '#262729' : '', height: isMobile ? '90vh' : '100vh', width: isMobile ? '100vw' : ''}}>
             <h2 className={`${darkMode ? 'text-white' :'text-black'} text-lg font-semibold column-header`}>Marketplace</h2>
                 <input
                     ref={inputRef}
