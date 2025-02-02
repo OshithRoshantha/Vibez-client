637c2d592228e6b1c2fd2fd4ffaae29a1a80381e
Oshith Roshantha <oedirisuriya@gmail.com>
2025-02-02 21:29:33 +0530

    refactor: simplify smart reply fetching by removing chat history dependency

--- src/Services/VibezIntelligence.ts
+++ src/Services/VibezIntelligence.ts
@@ -3,19 +3,11 @@ import axios from 'axios';
 const API = import.meta.env.VITE_API;
 const API2 = import.meta.env.VITE_API2;
 
-export const getSmartReply = async (chatHistory: string[]) => {
-    const response = await axios.post(`http://${API2}/vibez/ai_reply`, {
-        chatHistory: chatHistory
-    });
-    return response.data.reply;
-}
-
-export const getChatHistory = async (receiverId: string) => {
+export const getSmartReply = async (receiverId: string) => {
     const userId = sessionStorage.getItem('userId');
-    const response = await axios.get(`http://${API}/vibez/message/history/${userId}/${receiverId}`, {
-        headers: {
-            Authorization: `Bearer ${sessionStorage.getItem('token')}`,
-        },
+    const response = await axios.post(`http://${API2}/vibez/ai_reply`, {
+        userId,
+        receiverId
     });
     return response.data;
 }
